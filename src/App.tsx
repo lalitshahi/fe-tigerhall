0bf5a21460a38e4ab92b803796c1bf2e1981e43d
Lalit Shahi <shahilalit002@gmail.com>
2023-11-30 13:23:42 +0530

    feat: theme toggler feature added

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
-import { VStack, useColorModeValue } from "@chakra-ui/react";
+import {
+  HStack,
+  IconButton,
+  VStack,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import SearchBox from "./components/SearchBox";
 import ContentList from "./containers/ContentList";
 import { SearchProvider } from "./contexts/searchContext";
+import { Moon, Sun } from "react-feather";
 
 function App() {
   const bg = useColorModeValue("backgroundLight", "backgroundDark");
-
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <SearchProvider>
       <VStack p={5} gap={5} minH={"100vh"} bg={bg}>
-        <SearchBox />
+        <HStack w={"100%"}>
+          <SearchBox />
+          <IconButton
+            variant="outline"
+            colorScheme="tigerOrange"
+            aria-label={
+              colorMode === "light"
+                ? "Switch to Dark Theme"
+                : "Switch to Light Theme"
+            }
+            fontSize="20px"
+            icon={colorMode === "light" ? <Moon /> : <Sun />}
+            onClick={toggleColorMode}
+          />
+        </HStack>
         <ContentList />
       </VStack>
     </SearchProvider>
